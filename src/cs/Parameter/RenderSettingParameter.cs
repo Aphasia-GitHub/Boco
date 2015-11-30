342411c780d51dfd3689b713209e362549048ff5
zhengfei <zhengfei@boco.com.cn>
2015-11-30 20:03:46 +0800

    20:03

---
+++ src/cs/Parameter/RenderSettingParameter.cs
@@ -0,0 +1,18 @@
+﻿namespace Boco.Rnop.AppCore.Data
+{
+    public class SaveRenderSettingParameter
+    {
+        public long ID { get; set; }
+        public string UserID { get; set; }
+        public string RenderSetting { get; set; }
+        public string GroupName { get; set; }
+        public string Field { get; set; }
+        public string FieldAlias { get; set; }
+    }
+
+    public class GetRenderSettingParameter
+    {
+        public string GroupName { get; set; }
+        public string UserID { get; set; }
+    }
+}
