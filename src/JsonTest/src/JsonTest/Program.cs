69544e6bf49819835bc76188e8293db10c753f17
zhengfei <zhengfei@boco.com.cn>
2015-12-02 11:26:09 +0800

    TestJson

---
+++ src/JsonTest/src/JsonTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace JsonTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestJson();
+            Console.ReadKey();
+        }
+
+        static void TestJson()
+        {
+            var jsonObj = new
+            {
+                Text = "123",
+                Cols = new List<object>()
+            };
+            jsonObj.Cols.Add(new { ABC = "ABC1", SA = "SA1" });
+            jsonObj.Cols.Add(new { ABC = "ABC2", SA = "SA2" });
+
+            var str = JsonConvert.SerializeObject(jsonObj);
+            Console.WriteLine(str);
+        }
+    }
+}
