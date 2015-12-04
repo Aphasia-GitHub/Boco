a66530f47931b7c53e843687042d6522d9d884e3
zhengfei <zhengfei@boco.com.cn>
2015-12-04 14:46:33 +0800

    1、JsonTest 2、WebService

---
+++ src/cs/WebServiceDemo/WebServiceDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using ProxyClient;
+
+namespace WebServiceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new GetMsgToCreateSheetService();
+            var Paramstr = "<?xml version=\"1.0\" encoding=\"GBK\"?><root><data><request>";
+            var result = service.getMsgToCreateSheet(Paramstr);
+            System.Console.Read();
+        }
+    }
+}
